2e5ad7c18a7b31aafce2a4f37366fe964c3393f8
will787 <wvmwill@gmail.com>
2023-12-10 13:25:59 -0300

    introduzindo metodos em C#  e entendendo mais como funciona listas em C#

---
+++ Models/Curso.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace c__explore_concepts.Models
+{
+    public class Curso
+    {
+        public String Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+        public int ObterQuantidadeDeAlunosMatriculados(){
+            int quantidade = Alunos.Count;
+            return(quantidade);
+        }
+        public bool RemoverAluno(Pessoa aluno){
+            return Alunos.Remove(aluno);
+            //aqui temos um exemplo do método remover, devolve bool por conta do método trazer true
+            //executa o método recebendo argumento da lista que é o aluno
+        }
+        public void ListarAlunos(){
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine($"{aluno.NomeCompleto}");            
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+using c__explore_concepts.Models;
+
+
+
+
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "will";
+p1.Sobrenome = "vieira";
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Kuma";
+p2.Sobrenome = "Barto";
+
+Curso CursoDeIngles = new Curso();
+CursoDeIngles.Nome = "Inglês";
+CursoDeIngles.Alunos = new List<Pessoa>();
+//temos sempre que instanciar a listar quando tivermos um método lista<> na nossa classe usada
+
+CursoDeIngles.AdicionarAluno(p1);
+CursoDeIngles.AdicionarAluno(p2);
+CursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Idade = 19;
+// p1.Nome = "will";
+// p1.Sobrenome = "vieira";
